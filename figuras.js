6db2d9fdcc4268af63661405167258136ad4254f
Pedro Quiroga <pitterbck@gmail.com>
2021-07-22 14:41:35 -0300

    primer commit

---
+++ figuras.js
@@ -0,0 +1,11 @@
+// Curso práctico de JavaScript
+
+// Taller #1: Figuras geométricas
+/* 
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar la fórmula en JS
+- tercer paso: Crear funciones 
+- cuarto paso: Integrar JS con HTML
+*/
+
+console.log("Hello world");
